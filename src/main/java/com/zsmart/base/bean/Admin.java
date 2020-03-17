f3113e616dfd42b7be5b7df032b94b4dd08c646b
MoulaYounes <MoulaYounes@DESKTOP-GBNILO9>
2020-03-17 17:07:23 +0100

    reglage prob de id must be Long

--- src/main/java/com/zsmart/base/bean/Admin.java
+++ src/main/java/com/zsmart/base/bean/Admin.java
@@ -7,6 +7,8 @@ package com.zsmart.base.bean;
 
 import java.io.Serializable;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 /**
@@ -18,6 +20,8 @@ public class Admin implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String login;
     private String nom;
     private String phone;
@@ -81,5 +85,15 @@ public class Admin implements Serializable {
     public void setPhone(String phone) {
         this.phone = phone;
     }
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+    
+    
 
 }
